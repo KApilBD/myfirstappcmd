44b1f95b32bb51b87937fd3642aec2c2da489bf5
KApilBD <kapilbaraskar91@gmail.com>
2018-05-09 19:38:25 +0530

    React App

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+//import { BrowserRouter as Router} from "react-router-bootstrap";
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    
+    <Router>
+       <App/>
+    </Router>
+    ,
+ document.getElementById('root'));
+
+registerServiceWorker();
